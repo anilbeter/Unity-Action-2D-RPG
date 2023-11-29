5d2783822197de928098c4fc532ffbf3321ab769
Anil Beter <86671685+anilbeter@users.noreply.github.com>
2023-11-29 23:03:05 +0300

    add some variables to scriptable object

--- Assets/Scripts/Player/PlayerStats.cs
+++ Assets/Scripts/Player/PlayerStats.cs
@@ -5,5 +5,10 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "PlayerStats", menuName = "Player Stats")]
 public class PlayerStats : ScriptableObject
 {
+    [Header("Config")]
+    public int Level;
 
+    [Header("Health")]
+    public float Health;
+    public float MaxHealth;
 }

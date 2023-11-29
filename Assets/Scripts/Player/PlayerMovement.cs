584b3eaf7db40ace3b0556889df451209c94c47b
Anil Beter <86671685+anilbeter@users.noreply.github.com>
2023-11-29 10:50:15 +0300

    setup PlayerMovement script

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -4,15 +4,46 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float speed;
-    void Start()
-    {
-        
-    }
+    [Header("Config")]
+    [SerializeField] private float speed;
 
-    // Update is called once per frame
+    private PlayerActions actions;
+    private Rigidbody2D rb2D;
+    private Vector2 moveDirection;
+
+    private void Awake()
+    {
+        actions = new PlayerActions();
+        rb2D = GetComponent<Rigidbody2D>();
+    }
+    
     void Update()
     {
-        
+        ReadMovement();
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        rb2D.MovePosition(rb2D.position + moveDirection * (speed * Time.fixedDeltaTime));
+    }
+
+    private void ReadMovement()
+    {
+        moveDirection = actions.Movement.Move.ReadValue<Vector2>().normalized;
+    }
+
+    private void OnEnable()
+    {
+        actions.Enable();
+    }
+
+    private void OnDisable()
+    {
+        actions.Disable();
     }
 }

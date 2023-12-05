fd79d8882db82af99062e596ac0edc33b67b9f83
Anil Beter <86671685+anilbeter@users.noreply.github.com>
2023-12-05 18:03:41 +0300

    test UseMana w/ temp update method

--- Assets/Scripts/Player/PlayerMana.cs
+++ Assets/Scripts/Player/PlayerMana.cs
@@ -5,6 +5,14 @@ public class PlayerMana : MonoBehaviour
     [Header("Config")]
     [SerializeField] private PlayerStats stats;
 
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            UseMana(5f);
+        }
+    }
+
     public void UseMana(float amount)
     {
         stats.mana = Mathf.Max(stats.mana -= amount, 0f);

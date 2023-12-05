4ac4fba1ebfcf1b0ebf253fd8da52d3f7a8a94ee
Anil Beter <86671685+anilbeter@users.noreply.github.com>
2023-12-05 18:09:00 +0300

    add using UnityEditor namespace

--- Assets/Scripts/Player/Editor/PlayerStatsEditor.cs
+++ Assets/Scripts/Player/Editor/PlayerStatsEditor.cs
@@ -1,18 +1,8 @@
-using System.Collections;
-using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
-public class PlayerStatsEditor : MonoBehaviour
+public class PlayerStatsEditor : Editor
 {
     // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }

c10e461a9feb10adbfd326217b1045b92ed89e75
Anil Beter <86671685+anilbeter@users.noreply.github.com>
2023-12-05 17:56:43 +0300

    create UseMana method

--- Assets/Scripts/Player/PlayerMana.cs
+++ Assets/Scripts/Player/PlayerMana.cs
@@ -2,5 +2,23 @@
 
 public class PlayerMana : MonoBehaviour
 {
+    [Header("Config")]
+    [SerializeField] private PlayerStats stats;
 
+    public void UseMana(float amount)
+    {
+        /*
+        if (stats.mana >= amount)
+        {
+            stats.mana -= amount;
+            if (stats.mana <= 0f)
+            {
+                stats.mana = 0f;
+            }
+        }
+        */
+
+        // Shorter way:
+        stats.mana = Mathf.Max(stats.mana -= amount, 0f); // 2 değer girdim, büyük olanı vericek
+    }
 }

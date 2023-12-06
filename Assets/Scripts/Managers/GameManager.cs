d35f2e6f38b33687fec81930301eed6e13e9fb21
Anil Beter <86671685+anilbeter@users.noreply.github.com>
2023-12-06 21:13:10 +0300

    revive player via GameManager script

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -2,5 +2,13 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    [SerializeField] private Player player;
 
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            player.ResetPlayer();
+        }
+    }
 }

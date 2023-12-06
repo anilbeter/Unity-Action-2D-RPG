b7a173f57d24a53d52f28f5d2d0ffc600e724de5
Anil Beter <86671685+anilbeter@users.noreply.github.com>
2023-12-06 21:38:27 +0300

    setup player experience system

--- Assets/Scripts/Player/PlayerExperience.cs
+++ Assets/Scripts/Player/PlayerExperience.cs
@@ -4,5 +4,32 @@ using UnityEngine;
 
 public class PlayerExperience : MonoBehaviour
 {
+    [Header("Config")]
+    [SerializeField] private PlayerStats stats;
 
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            AddExperience(300f);
+        } 
+    }
+    public void AddExperience(float amount)
+    {
+        stats.currentExp += amount;
+        while (stats.currentExp >= stats.nextLevelExp)
+        {
+            stats.currentExp -= stats.nextLevelExp;
+            NextLevel();
+        }
+    }
+
+    private void NextLevel()
+    {
+        stats.Level++;
+        float currentExpRequired = stats.nextLevelExp;
+        float newNextLevelExp = Mathf.Round(currentExpRequired + stats.nextLevelExp * (stats.expMultiplier / 100f));
+        stats.nextLevelExp = newNextLevelExp;
+    }
 }

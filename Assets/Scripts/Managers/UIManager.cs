84a2a08c8f43974b41bf55762bb00ce6bd6d20a0
Anil Beter <86671685+anilbeter@users.noreply.github.com>
2023-12-14 23:17:44 +0300

    update player UI w/ UIManager.cs

--- Assets/Scripts/Managers/UIManager.cs
+++ Assets/Scripts/Managers/UIManager.cs
@@ -1,18 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [Header("Stats")]
+    [SerializeField] private PlayerStats stats;
+
+    [Header("Bars")]
+    [SerializeField] private Image healthBar;
+    [SerializeField] private Image manaBar;
+    [SerializeField] private Image expBar;
+
+    [Header("Text")]
+    [SerializeField] private TextMeshProUGUI levelTMP;
+    [SerializeField] private TextMeshProUGUI healthTMP;
+    [SerializeField] private TextMeshProUGUI manaTMP;
+    [SerializeField] private TextMeshProUGUI expTMP;
+
+    private void Update()
     {
-        
+        UpdatePlayerUI();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void UpdatePlayerUI()
     {
-        
+        healthBar.fillAmount = Mathf.Lerp(healthBar.fillAmount, stats.health / stats.maxHealth, 10f * Time.deltaTime);
+        manaBar.fillAmount = Mathf.Lerp(manaBar.fillAmount, stats.mana / stats.maxMana, 10f * Time.deltaTime);
+        expBar.fillAmount = Mathf.Lerp(expBar.fillAmount, stats.currentExp / stats.nextLevelExp, 10f * Time.deltaTime);
+
+        levelTMP.text = $"Level {stats.Level}";
+        healthTMP.text = $"{stats.health} / {stats.maxHealth}";
+        manaTMP.text = $"{stats.mana} / {stats.maxMana}";
+        expTMP.text = $"{stats.currentExp} / {stats.nextLevelExp}";
     }
 }
